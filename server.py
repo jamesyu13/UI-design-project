f28c900479102fabd031c689cadb2d25f5e186e8
JittisaK11 <jittisak2003@gmail.com>
2024-04-16 14:08:19 -0400

    added quiz slides (questions 1-3)

--- server.py
+++ server.py
@@ -10,5 +10,15 @@ import logging
 def main():
    return render_template('index.html')
 
+@app.route('/quiz', methods=['GET', 'POST'])
+def quiz():
+   return render_template('quiz.html')
+
+@app.route('/quiz<int:id>', methods=['GET', 'POST'])
+def quiz_id(id):
+    # Constructs the filename based on the quiz ID
+    quiz_file = f'quiz{id}.html'
+    return render_template(quiz_file)
+
 if __name__ == '__main__':
    app.run(debug = True)
